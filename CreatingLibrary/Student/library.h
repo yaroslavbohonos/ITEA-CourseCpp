81becb499bf1a08cb5ff0b5feb448f8fd2927a35
Yaroslav Bohonos <102082030+yaroslavbohonos@users.noreply.github.com>
2023-07-05 16:09:00 +0100

    Create library.h

---
+++ CreatingLibrary/Student/library.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Student
+{
+  int age, number;
+public:
+  Student();
+  void set(int new_age, int new_number);
+  int GetAge() { return age; }
+  int GetA() { return age; }
+  void Print();
+  ~Student();
+};

d3cc1ef880a66684456c77faf8e5e31d826ecc01
Yaroslav Bohonos <102082030+yaroslavbohonos@users.noreply.github.com>
2023-07-05 16:08:26 +0100

    Create main.cpp

---
+++ CreatingLibrary/Student/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#include "library.h"
+
+int main() {
+  int age, number;
+  cout << "Enter student age and number:";
+  cin>>age;
+  cin>>number;
+  Student student1;
+  student1.set(age, number);
+  student1.Print();
+}

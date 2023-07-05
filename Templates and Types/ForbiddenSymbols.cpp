48603d25725c4b7bf90629124611ffdb778a65e8
Yaroslav Bohonos <102082030+yaroslavbohonos@users.noreply.github.com>
2023-07-05 16:19:48 +0100

    Update ForbiddenSymbols.cpp

--- Templates and Types/ForbiddenSymbols.cpp
+++ Templates and Types/ForbiddenSymbols.cpp
@@ -4,45 +4,55 @@ Task: Create a class that can only contain the following characters: '₴1234567
 If an attempt is made to enter any other symbol into the array, handle it by displaying an appropriate message on the screen.
 Try to construct an expression from the allowed elements of the array and output it to the screen. */
 
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector>
 
-class Vector {
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T> class Vector
+{
 protected:
-  std::vector<std::string> myvector;
-  std::vector<std::string> forbiddenSymbols{
-      "`", "₴", "1", "2", "3",  "4", "5",  "6", "7", "8", "9",
-      "0", "-", "=", "!", "\"", "№", "#",  ";", "%", ":", "?",
-      "(", ")", "_", "+", "/",  "*", "\\", ",", "."};
+  vector <T> myvector;
+  vector <T> Forbidden {
+  "`", "₴", "1", "2", "3",
+  "4", "5", "6", "7", "8",
+  "9", "0", "-", "=", "!",
+  "\"", "№", "#", ";", "%",
+  ":", "?", "(", ")", "_",
+  "+", "/", "*", "\\", ",", "."
+  };
 
 public:
-  bool isForbiddenSymbol(const std::string &symbol) {
-    return std::find(forbiddenSymbols.begin(), forbiddenSymbols.end(),
-                     symbol) != forbiddenSymbols.end();
+  bool isForbiddenSymbols(T symbol)
+  {
+    for(int i = 0; i < Forbidden.size(); i++)
+      if(Forbidden.at(i) == symbol)
+        return true;
+    return false;
   }
 
-  void push(const std::string &symbol) {
-    if (isForbiddenSymbol(symbol)) {
+  void push(T symbol)
+  {
+    if(isForbiddenSymbols(symbol))
       myvector.push_back(symbol);
-    } else {
-      std::cout << "The symbol: " << symbol << " is forbidden\n";
-    }
+    else cout << "The symbol: " << symbol << " is forbidden\n";
   }
 
-  friend std::ostream &operator<<(std::ostream &out, const Vector &vect) {
-    if (!vect.myvector.empty()) {
-      for (const auto &symbol : vect.myvector) {
-        out << symbol << " ";
-      }
+  friend ostream& operator<< (ostream &out, Vector<string> &vect)
+  {
+    if(!vect.myvector.empty()) 
+    {
+      for(int i = 0; i < vect.myvector.size(); i++)
+        cout << vect.myvector.at(i) << " ";
+      return out;
     }
     return out;
   }
 };
 
-int main() {
-  Vector A;
+int main()
+{
+  Vector<string> A;  
   A.push("3");
   A.push("/");
   A.push("10");
@@ -51,7 +61,5 @@ int main() {
   A.push("3");
   A.push("=");
   A.push("1");
-  std::cout << A << std::endl;
-
-  return 0;
+  cout << A;
 }
